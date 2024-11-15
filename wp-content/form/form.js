d847f0c1020c318f7452b5e53a63f6ada882a3fd
= <mn456897@gmail.com>
2024-11-15 18:54:24 +0700

    upload images from client

--- wp-content/form/form.js
+++ wp-content/form/form.js
@@ -1,139 +1,187 @@
-const provice_url = "https://api.npoint.io/ac646cb54b295b9555be";
-const district_url = "https://api.npoint.io/34608ea16bebc5cffd42";
-const ward_url = "https://api.npoint.io/dd278dc276e65c68cdf5";
+const provice_url = 'https://api.npoint.io/ac646cb54b295b9555be';
+const district_url = 'https://api.npoint.io/34608ea16bebc5cffd42';
+const ward_url = 'https://api.npoint.io/dd278dc276e65c68cdf5';
 
 var province_list = [];
 var district_list = [];
 var ward_list = [];
 
 $(document).ready(function () {
-  getAllDataForDropdown();
-  initProviceDropdown();
-  hanldeOnchangeProvice();
-  hanldeOnchangeDistrict();
-  hanldeButtonGetData();
+	getAllDataForDropdown();
+	initProviceDropdown();
+	hanldeOnchangeProvice();
+	hanldeOnchangeDistrict();
+	hanldeButtonGetData();
 });
 
-var getDataSelected = function(){
-    return {
-        ProvinceText: $('#Province').find("option:selected").text(),
-        ProvinceId:  $('#Province').val(),
-        DistrictText:  $('#District').find("option:selected").text(),
-        DistrictId:  $('#District').val(),
-        WardText:  $('#Ward').find("option:selected").text(),
-        WardId:  $('#Ward').val()
-    }
-}
+var getDataSelected = function () {
+	return {
+		ProvinceText: $('#Province').find('option:selected').text(),
+		ProvinceId: $('#Province').val(),
+		DistrictText: $('#District').find('option:selected').text(),
+		DistrictId: $('#District').val(),
+		WardText: $('#Ward').find('option:selected').text(),
+		WardId: $('#Ward').val()
+	};
+};
 
-var hanldeButtonGetData = function(){
-    $("#BtnGetDataSelected").on('click', function(e){
-        e.preventDefault();
-        let data = getDataSelected();
-        $('#DataText').html('');
-        $('#DataId').html('');
+var hanldeButtonGetData = function () {
+	$('#BtnGetDataSelected').on('click', function (e) {
+		e.preventDefault();
+		let data = getDataSelected();
+		$('#DataText').html('');
+		$('#DataId').html('');
 
-        $('#DataText').append('<li>Tỉnh/Thành Phố: <strong>' + data.ProvinceText + '</strong></li>');
-        $('#DataText').append('<li>Quận/Huyện: <strong>' + data.DistrictText + '</strong></li>');
-        $('#DataText').append('<li>Xã/Phường: <strong>' + data.WardText + '</strong></li>');
+		$('#DataText').append('<li>Tỉnh/Thành Phố: <strong>' + data.ProvinceText + '</strong></li>');
+		$('#DataText').append('<li>Quận/Huyện: <strong>' + data.DistrictText + '</strong></li>');
+		$('#DataText').append('<li>Xã/Phường: <strong>' + data.WardText + '</strong></li>');
 
-        $('#DataId').append('<li>Province Id: <strong>' + data.ProvinceId + '</strong></li>');
-        $('#DataId').append('<li>District Id: <strong>' + data.DistrictId + '</strong></li>');
-        $('#DataId').append('<li>Ward Id: <strong>' + data.WardId + '</strong></li>');
-    });
-}
+		$('#DataId').append('<li>Province Id: <strong>' + data.ProvinceId + '</strong></li>');
+		$('#DataId').append('<li>District Id: <strong>' + data.DistrictId + '</strong></li>');
+		$('#DataId').append('<li>Ward Id: <strong>' + data.WardId + '</strong></li>');
+	});
+};
 
 var hanldeOnchangeProvice = function () {
-  $("#Province").on("change", function () {
-    var id = $(this).val();
-    if (id) {
-      
-      $("#District").empty();
+	$('#Province').on('change', function () {
+		var id = $(this).val();
+		if (id) {
+			$('#District').empty();
 
-      var data_filter = district_list.filter(
-        (entry) => entry.ProvinceId === parseInt(id)
-      );
-      
-      singleSelectDropdown("District", "Chọn Quận/Huyện", data_filter);
-    }
-  });
+			var data_filter = district_list.filter((entry) => entry.ProvinceId === parseInt(id));
+
+			singleSelectDropdown('District', 'Chọn Quận/Huyện', data_filter);
+		}
+	});
 };
 
 var hanldeOnchangeDistrict = function () {
-    $("#District").on("change", function () {
-      var id = $(this).val();
-      if (id) {
-        
-        $("#Ward").empty();
-  
-        var data_filter = ward_list.filter(
-          (entry) => entry.DistrictId === parseInt(id)
-        );
-        
-        singleSelectDropdown("Ward", "Chọn Xã/Phường", data_filter);
-      }
-    });
+	$('#District').on('change', function () {
+		var id = $(this).val();
+		if (id) {
+			$('#Ward').empty();
+
+			var data_filter = ward_list.filter((entry) => entry.DistrictId === parseInt(id));
+
+			singleSelectDropdown('Ward', 'Chọn Xã/Phường', data_filter);
+		}
+	});
 };
 
 var initProviceDropdown = function () {
-  var interval = setInterval(function () {
-    if (province_list.length > 0) {
-      singleSelectDropdown("Province", "Chọn Tỉnh/Thành Phố", province_list);
-      clearInterval(interval);
-    }
-  }, 100);
+	var interval = setInterval(function () {
+		if (province_list.length > 0) {
+			singleSelectDropdown('Province', 'Chọn Tỉnh/Thành Phố', province_list);
+			clearInterval(interval);
+		}
+	}, 100);
 };
 
 var getAllDataForDropdown = function () {
-  getProviceData(function (list) {
-    province_list = list;
-  });
+	getProviceData(function (list) {
+		province_list = list;
+	});
 
-  getDistrictData(function (list) {
-    district_list = list;
-  });
+	getDistrictData(function (list) {
+		district_list = list;
+	});
 
-  getWardData(function (list) {
-    ward_list = list;
-  });
+	getWardData(function (list) {
+		ward_list = list;
+	});
 };
 
 var getProviceData = function (callback) {
-  $.getJSON(provice_url, function (list) {
-    callback(list);
-  });
+	$.getJSON(provice_url, function (list) {
+		callback(list);
+	});
 };
 
 var getDistrictData = function (callback) {
-  $.getJSON(district_url, function (list) {
-    callback(list);
-  });
+	$.getJSON(district_url, function (list) {
+		callback(list);
+	});
 };
 
 var getWardData = function (callback) {
-  $.getJSON(ward_url, function (list) {
-    callback(list);
-  });
+	$.getJSON(ward_url, function (list) {
+		callback(list);
+	});
 };
 
 var singleSelectDropdown = function (id, placeholder, data, selectItem) {
-  setTimeout(function () {
-    $("#" + id).select2({
-      placeholder: placeholder,
-      data: data.map(function (item) {
-        return {
-          id: item.Id,
-          text: item.Name,
-        };
-      }),
-      allowClear: true,
-    });
-  }, 200);
+	setTimeout(function () {
+		$('#' + id).select2({
+			placeholder: placeholder,
+			data: data.map(function (item) {
+				return {
+					id: item.Id,
+					text: item.Name
+				};
+			}),
+			allowClear: true
+		});
+	}, 200);
 
-  if (selectItem !== null) {
-    setTimeout(function () {
-      $("#" + id)
-        .val(selectItem)
-        .trigger("change");
-    }, 200);
-  }
+	if (selectItem !== null) {
+		setTimeout(function () {
+			$('#' + id)
+				.val(selectItem)
+				.trigger('change');
+		}, 200);
+	}
 };
+
+//
+$(document).ready(function () {
+	$('#uploadImage').on('change', function (e) {
+		e.preventDefault();
+		// Lấy danh sách các file từ input
+		const files = this.files;
+
+		// Kiểm tra nếu có file nào được chọn
+		if (files.length > 0) {
+			$.each(files, function (index, file) {
+				// Tạo FormData để gửi file qua API
+				const formData = new FormData();
+				formData.append('file', file);
+
+				// Gửi API tới server
+				$.ajax({
+					url: 'http://localhost:8080/api/files/images', // Đường dẫn API của server
+					type: 'POST',
+					data: formData,
+					contentType: false, // Bắt buộc phải đặt false để gửi FormData
+					processData: false, // Không xử lý dữ liệu
+					success: function (response) {
+						// Nhận link URL từ server (giả định response trả về URL)
+						const imageUrl = response.result; // Giả định response có { data: url }
+
+						// Tạo thẻ div để chứa ảnh xem trước và nút xóa
+						const imgContainer = $(`
+                      <div style="padding: 2px 8px; position: relative;">
+                          <span class="remove-image" style="position: absolute; top: 15%; right: 10%; color: #000; font-weight: 700; cursor: pointer;">X</span>
+                          <img src="${imageUrl}" alt="Ảnh xem trước" style="width: 100px; height: 100px; border-radius: 2px;">
+                      </div>
+                  `);
+
+						// Thêm thẻ ảnh vào `.images-preview`
+						$('.images-preview').append(imgContainer);
+					},
+					error: function (xhr, status, error) {
+						console.error('Lỗi khi tải lên file:', error);
+						alert('Tải ảnh lên server thất bại.');
+					}
+				});
+			});
+		}
+
+		// Reset input file để có thể chọn cùng file lần sau
+		$(this).val('');
+	});
+
+	// Xóa ảnh khi nhấn nút X
+	$(document).on('click', '.remove-image', function () {
+		$(this).parent().remove();
+	});
+});
--- wp-content/login/script.js
+++ wp-content/login/script.js
@@ -1,60 +1,53 @@
-import {
-  postUser,
-  login,
-  ADMIN_ROLE,
-  USER_ROLE,
-  URLIndex,
-  postORG,
-} from "../../common/common.js";
+import { postUser, login, ADMIN_ROLE, USER_ROLE, URLIndex, postORG } from '../../common/common.js';
 
 $(document).ready(function () {
-  // =============== Change view =====================
-  // Chuyển sang form đăng nhập
-  $("#switch-to-login, #signIn").click(function () {
-    $(".sign-in-container").css("left", "0");
-    $(".sign-up-container").css("left", "100%");
-    $(".overlay-container").removeClass("active");
-  });
+	// =============== Change view =====================
+	// Chuyển sang form đăng nhập
+	$('#switch-to-login, #signIn').click(function () {
+		$('.sign-in-container').css('left', '0');
+		$('.sign-up-container').css('left', '100%');
+		$('.overlay-container').removeClass('active');
+	});
 
-  // Chuyển sang form đăng ký người dùng
-  $("#switch-to-register, #switch-to-user").click(function () {
-    $("#register-organization").css("opacity", "0");
-    $("#register-organization").css("z-index", "-1");
+	// Chuyển sang form đăng ký người dùng
+	$('#switch-to-register, #switch-to-user').click(function () {
+		$('#register-organization').css('opacity', '0');
+		$('#register-organization').css('z-index', '-1');
 
-    $("#register-user").css("opacity", "1");
-    $("#register-user").css("z-index", "1");
+		$('#register-user').css('opacity', '1');
+		$('#register-user').css('z-index', '1');
 
-    $(".sign-up-container").removeClass("hidden");
-    $(".sign-in-container").css("left", "-100%");
-    $(".sign-up-container").css("left", "0");
-    $(".overlay-container").addClass("active");
-  });
+		$('.sign-up-container').removeClass('hidden');
+		$('.sign-in-container').css('left', '-100%');
+		$('.sign-up-container').css('left', '0');
+		$('.overlay-container').addClass('active');
+	});
 
-  // Chuyển sang form đăng ký tổ chức
-  $("#switch-to-org").click(function () {
-    $("#register-organization").css("opacity", "1");
-    $("#register-organization").css("z-index", "1");
+	// Chuyển sang form đăng ký tổ chức
+	$('#switch-to-org').click(function () {
+		$('#register-organization').css('opacity', '1');
+		$('#register-organization').css('z-index', '1');
 
-    $("#register-user").css("opacity", "0");
-    $("#register-user").css("z-index", "-1");
+		$('#register-user').css('opacity', '0');
+		$('#register-user').css('z-index', '-1');
 
-    $(".sign-in-container").css("left", "-100%");
-    $(".sign-up-container").css("left", "0");
-    $(".overlay-container").addClass("active");
-  });
+		$('.sign-in-container').css('left', '-100%');
+		$('.sign-up-container').css('left', '0');
+		$('.overlay-container').addClass('active');
+	});
 
-  // Quay lại đăng ký người dùng từ đăng ký tổ chức
-  $("#switch-back-to-user").click(function () {
-    $("#register-organization").css("opacity", "0");
-    $("#register-organization").css("z-index", "-1");
+	// Quay lại đăng ký người dùng từ đăng ký tổ chức
+	$('#switch-back-to-user').click(function () {
+		$('#register-organization').css('opacity', '0');
+		$('#register-organization').css('z-index', '-1');
 
-    $("#register-user").css("opacity", "1");
-    $("#register-user").css("z-index", "1");
-  });
+		$('#register-user').css('opacity', '1');
+		$('#register-user').css('z-index', '1');
+	});
 
-  // ============ Set role when click radio button ================
-  const roleSelection = document.querySelector(".role-selection");
-  roleSelection.innerHTML = `
+	// ============ Set role when click radio button ================
+	const roleSelection = document.querySelector('.role-selection');
+	roleSelection.innerHTML = `
       <label>
           <input type="radio" name="login-role" value="${USER_ROLE}" required />
           <span class="radio-label">Người dùng</span>
@@ -65,54 +58,53 @@ $(document).ready(function () {
       </label>
   `;
 
-  //============= Phone ===============
-  const phoneInput = document.getElementById("user-phone");
+	//============= Phone ===============
+	const phoneInput = document.getElementById('user-phone');
 
-  phoneInput.addEventListener("input", () => {
-    let phone = phoneInput.value;
-    let regex = /^(0|\+84)[0-9]{9}$/;
+	phoneInput.addEventListener('input', () => {
+		let phone = phoneInput.value;
+		let regex = /^(0|\+84)[0-9]{9}$/;
 
-    if (!regex.test(phone)) {
-      phoneInput.setCustomValidity("Số điện thoại không hợp lệ.");
-    } else {
-      phoneInput.setCustomValidity("");
-    }
-  });
+		if (!regex.test(phone)) {
+			phoneInput.setCustomValidity('Số điện thoại không hợp lệ.');
+		} else {
+			phoneInput.setCustomValidity('');
+		}
+	});
 
-  // ================= CCCD ==============
-  const cccdInput = document.getElementById("user-cccd");
+	// ================= CCCD ==============
+	const cccdInput = document.getElementById('user-cccd');
 
-  cccdInput.addEventListener("input", () => {
-    const cccd = cccdInput.value;
-    const regex = /^(0)[0-9]{11}$/;
+	cccdInput.addEventListener('input', () => {
+		const cccd = cccdInput.value;
+		const regex = /^(0)[0-9]{11}$/;
 
-    if (!regex.test(cccd)) {
-      cccdInput.setCustomValidity("Vui lòng nhập chính xác CCCD của bạn.");
-    } else {
-      cccdInput.setCustomValidity("");
-    }
-  });
+		if (!regex.test(cccd)) {
+			cccdInput.setCustomValidity('Vui lòng nhập chính xác CCCD của bạn.');
+		} else {
+			cccdInput.setCustomValidity('');
+		}
+	});
 
-  // ===============  password ======================
-  const passwordInput = document.getElementById("user-password");
+	// ===============  password ======================
+	const passwordInput = document.getElementById('user-password');
 
-  passwordInput.addEventListener("input", () => {
-    const password = passwordInput.value;
-    const regex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+	passwordInput.addEventListener('input', () => {
+		const password = passwordInput.value;
+		const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
 
-    if (!regex.test(password)) {
-      passwordInput.setCustomValidity(
-        "Mật khẩu của bạn phải chứa ít nhất 6 kí tự\n ít nhất một chữ số \nmột chữ thường, một chữ hoa \n và một ký tự đặc biệt. "
-      );
-    } else {
-      passwordInput.setCustomValidity("");
-    }
-  });
+		if (!regex.test(password)) {
+			passwordInput.setCustomValidity(
+				'Mật khẩu của bạn phải chứa ít nhất 6 kí tự\n ít nhất một chữ số \nmột chữ thường, một chữ hoa \n và một ký tự đặc biệt. '
+			);
+		} else {
+			passwordInput.setCustomValidity('');
+		}
+	});
 
-  //===============  Register USER===============
+	//===============  Register USER===============
 
-  /*
+	/*
   "phoneNumber": "0374798126",
   "password": "thang2506",
   "fullName": "conanWinner",
@@ -122,151 +114,151 @@ $(document).ready(function () {
 
 */
 
-  $("#register-user-form").submit(function (e) {
-    e.preventDefault();
-    const name = $("#user-name").val();
-    const phoneNumber = $("#user-phone").val();
-    const password = $("#user-password").val();
-    const cccd = $("#user-cccd").val();
-    const address = $("#user-address").val();
-    const role_name = "USER";
+	$('#register-user-form').submit(function (e) {
+		e.preventDefault();
+		const name = $('#user-name').val();
+		const phoneNumber = $('#user-phone').val();
+		const password = $('#user-password').val();
+		const cccd = $('#user-cccd').val();
+		const address = $('#user-address').val();
+		const role_name = 'USER';
 
-    console.log(name, phoneNumber, password, cccd, address);
+		console.log(name, phoneNumber, password, cccd, address);
 
-    $.ajax({
-      url: postUser,
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
-        phoneNumber: phoneNumber,
-        password: password,
-        fullName: name,
-        cccd: cccd,
-        address: address,
-        role_name: role_name,
-      }),
-      success: function (response) {
-        console.log(response);
-        alert("Đăng ký thành công!, vui lòng đăng nhập.");
-        $("#user-phone").val("");
-        $("#user-password").val("");
-        $("#user-name").val("");
-        $("#user-cccd").val("");
-        $("#user-address").val("");
-      },
-      error: function (error) {
-        console.log(error);
-        alert("Có lỗi xảy ra khi đăng ký!");
-      },
-    });
-  });
+		$.ajax({
+			url: postUser,
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				phoneNumber: phoneNumber,
+				password: password,
+				fullName: name,
+				cccd: cccd,
+				address: address,
+				role_name: role_name
+			}),
+			success: function (response) {
+				console.log(response);
+				alert('Đăng ký thành công!, vui lòng đăng nhập.');
+				$('#user-phone').val('');
+				$('#user-password').val('');
+				$('#user-name').val('');
+				$('#user-cccd').val('');
+				$('#user-address').val('');
+			},
+			error: function (error) {
+				console.log(error);
+				alert('Có lỗi xảy ra khi đăng ký!');
+			}
+		});
+	});
 
-  //===============  Register ORG===============
+	//===============  Register ORG===============
 
-  // {
-  //   "phoneNumber": "",
-  //   "password": "",
-  //   "organizationName": "",
-  //   "representativeName": "",
-  //   "cccd": "",
-  //   "address": "",
-  //   "description": "",
-  //   "role_name": ""
-  // }
+	// {
+	//   "phoneNumber": "",
+	//   "password": "",
+	//   "organizationName": "",
+	//   "representativeName": "",
+	//   "cccd": "",
+	//   "address": "",
+	//   "description": "",
+	//   "role_name": ""
+	// }
 
-  $("#register-organization-form").submit(function (e) {
-    e.preventDefault();
-    const phoneNumber = $("#org-phone").val();
-    const password = $("#org-password").val();
-    const organizationName = $("#org-representation_name").val();
-    const representativeName = $("#org-name").val();
-    const cccd = $("#org-cccd").val();
-    const address = $("#org-address").val();
-    const description = $("#org-description").val();
-    const role_name = "ADMIN";
+	$('#register-organization-form').submit(function (e) {
+		e.preventDefault();
+		const phoneNumber = $('#org-phone').val();
+		const password = $('#org-password').val();
+		const organizationName = $('#org-representation_name').val();
+		const representativeName = $('#org-name').val();
+		const cccd = $('#org-cccd').val();
+		const address = $('#org-address').val();
+		const description = $('#org-description').val();
+		const role_name = 'ADMIN';
 
-    console.log(
-      phoneNumber,
-      password,
-      organizationName,
-      representativeName,
-      cccd,
-      address,
-      description,
-      role_name
-    );
+		console.log(
+			phoneNumber,
+			password,
+			organizationName,
+			representativeName,
+			cccd,
+			address,
+			description,
+			role_name
+		);
 
-    $.ajax({
-      url: postORG,
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({
-        phoneNumber,
-        password,
-        organizationName,
-        representativeName,
-        cccd,
-        address,
-        description,
-        role_name,
-      }),
-      success: function (response) {
-        console.log(response);
-        alert("Đăng ký thành công!, vui lòng đăng nhập.");
-        $("#org-phone").val("");
-        $("#org-password").val("");
-        $("#org-representation_name").val("");
-        $("#org-name").val("");
-        $("#org-cccd").val("");
-        $("#org-address").val("");
-        $("#org-description").val("");
-      },
-      error: function (error) {
-        console.log(error);
-        alert("Có lỗi xảy ra khi đăng ký!");
-      },
-    });
-  });
+		$.ajax({
+			url: postORG,
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({
+				phoneNumber,
+				password,
+				organizationName,
+				representativeName,
+				cccd,
+				address,
+				description,
+				role_name
+			}),
+			success: function (response) {
+				console.log(response);
+				alert('Đăng ký thành công!, vui lòng đăng nhập.');
+				$('#org-phone').val('');
+				$('#org-password').val('');
+				$('#org-representation_name').val('');
+				$('#org-name').val('');
+				$('#org-cccd').val('');
+				$('#org-address').val('');
+				$('#org-description').val('');
+			},
+			error: function (error) {
+				console.log(error);
+				alert('Có lỗi xảy ra khi đăng ký!');
+			}
+		});
+	});
 
-  // ==============   Login  ===============
-  $("#login-form").submit(function (e) {
-    e.preventDefault();
-    const phoneNumber = $("#login-phone").val();
-    const password = $("#login-password").val();
-    const whoAreYou = $('input[name="login-role"]:checked').val();
+	// ==============   Login  ===============
+	$('#login-form').submit(function (e) {
+		e.preventDefault();
+		const phoneNumber = $('#login-phone').val();
+		const password = $('#login-password').val();
+		const whoAreYou = $('input[name="login-role"]:checked').val();
 
-    if (!whoAreYou) {
-      alert("Vui lòng chọn vai trò đăng nhập!");
-    } else {
-      console.log(`Đăng nhập với vai trò: ${whoAreYou}`);
-    }
-    $.ajax({
-      url: login,
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({ phoneNumber, password, whoAreYou }),
-      success: function (response) {
-        const data = response.result;
-        const userData = data.result;
+		if (!whoAreYou) {
+			alert('Vui lòng chọn vai trò đăng nhập!');
+		} else {
+			console.log(`Đăng nhập với vai trò: ${whoAreYou}`);
+		}
+		$.ajax({
+			url: login,
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({ phoneNumber, password, whoAreYou }),
+			success: function (response) {
+				const data = response.result;
+				const userData = data.result;
 
-        console.log(userData);
-        console.log(JSON.stringify(data, null, 2));
+				console.log(userData);
+				console.log(JSON.stringify(data, null, 2));
 
-        var fullName = userData.fullName;
-        var iduser = userData.id;
+				var fullName = userData.fullName;
+				var iduser = userData.id;
 
-        localStorage.setItem("iduser", iduser);
-        localStorage.setItem("fullName", fullName);
-        localStorage.setItem("phoneNumber", phoneNumber);
-        localStorage.setItem("roleName", whoAreYou);
+				localStorage.setItem('iduser', iduser);
+				localStorage.setItem('fullName', fullName);
+				localStorage.setItem('phoneNumber', phoneNumber);
+				localStorage.setItem('roleName', whoAreYou);
 
-        alert("Đăng nhập thành công!");
-        window.location.href = URLIndex;
-      },
-      error: function (error) {
-        console.log(error);
-        alert("Sai thông tin đăng nhập!");
-      },
-    });
-  });
+				alert('Đăng nhập thành công!');
+				window.location.href = URLIndex;
+			},
+			error: function (error) {
+				console.log(error);
+				alert('Sai thông tin đăng nhập!');
+			}
+		});
+	});
 });
